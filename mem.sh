b9b623807522e921b6c219ba4a7751086f845d15
mrabie19 <m.rabie-19@lboro.ac.uk>
2023-12-19 16:55:35 +0000

    basic script to show memory usage of all elf files

---
+++ mem.sh
@@ -0,0 +1,30 @@
+arm-none-eabi-size Software/AES/AES.elf
+echo
+arm-none-eabi-size Software/ascon128/ascon128.elf
+echo
+arm-none-eabi-size Software/ascon128a/ascon128a.elf
+echo
+arm-none-eabi-size Software/elephant160v2/elephant160v2.elf
+echo
+arm-none-eabi-size Software/giftcofb128v1/giftcofb128v1.elf
+echo
+arm-none-eabi-size Software/grain128aeadv2/grain128aeadv2.elf
+echo
+arm-none-eabi-size Software/isapa128av20/isapa128av20.elf
+echo
+arm-none-eabi-size Software/isapa128v20/isapa128v20.elf
+echo
+arm-none-eabi-size Software/photonbeetleaead128rate128v1/photonbeetleaead128rate128v1.elf
+echo
+arm-none-eabi-size Software/romulusn/romulusn.elf
+echo
+arm-none-eabi-size Software/schwaemm256128v2/schwaemm256128v2.elf
+echo
+arm-none-eabi-size Software/schwaemm256256v2/schwaemm256256v2.elf
+echo
+arm-none-eabi-size Software/tinyjambu/tinyjambu.elf
+echo
+arm-none-eabi-size Software/xoodyak/xoodyak.elf
+echo
+
+$SHELL #(prevent shell from auto closing)
