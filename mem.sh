6dfa2a5b492d7fce08f1cbba2768632bda5d5f5f
mrabie9 <md.rabie98@gmail.com>
2024-05-09 08:39:04 +0100

    minor adjustments to scripts

--- mem.sh
+++ mem.sh
@@ -1,58 +1,8 @@
-echo "AES:" | tee mem_usage.txt
-arm-none-eabi-size Software/AES/AES.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "ascon128:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/ascon128/ascon128.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "ascon128a:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/ascon128a/ascon128a.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "elephant160v2:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/elephant160v2/elephant160v2.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "giftcofb128v1:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/giftcofb128v1/giftcofb128v1.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "grain128aeadv2:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/grain128aeadv2/grain128aeadv2.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "isapa128av20:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/isapa128av20/isapa128av20.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "isapa128v20:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/isapa128v20/isapa128v20.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "photonbeetleaead128rate128v1:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/photonbeetleaead128rate128v1/photonbeetleaead128rate128v1.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "romulusn:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/romulusn/romulusn.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "schwaemm256128v2:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/schwaemm256128v2/schwaemm256128v2.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "schwaemm256256v2:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/schwaemm256256v2/schwaemm256256v2.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-echo "tinyjambu:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/tinyjambu/tinyjambu.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt	
-
-echo "xoodyak:" | tee -a mem_usage.txt
-arm-none-eabi-size Software/xoodyak/xoodyak.elf | tee -a mem_usage.txt
-echo | tee -a mem_usage.txt
-
-
+arm-none-eabi-size Build/ascon128_O2.elf 
+arm-none-eabi-size Build/ascon128Armv7_O2.elf
+arm-none-eabi-size Build/ascon128a_O2.elf 
+arm-none-eabi-size Build/ascon128aArmv7_O2.elf
+arm-none-eabi-size Build/schwaemm256128v2_O2.elf 
+arm-none-eabi-size Build/schwaemm256128v2Armv7_O2.elf 
+arm-none-eabi-size Build/schwaemm256256v2Armv7_O2.elf 
 $SHELL #(prevent shell from auto closing)
